748c8665420739db930efbec69768efe3d29ce7d
“Vimalate <1902644369@qq.com>
2020-01-08 02:41:39 +0800

    导航栏

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,7 +1,35 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <el-container class="home_container">
+    <el-header>
+      <div>
+        <img src="../assets/heima.png" alt />
+        <span>超级管理后台</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">
+        <!-- 侧边栏菜单区域 -->
+        <el-menu background-color="#333744" text-color="#fff" active-text-color="#ffd04b">
+          <!-- 一级菜案 -->
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>导航一</span>
+            </template>
+            <!-- 二级菜单 -->
+            <el-menu-item index="1-4-1">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>导航一</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -18,4 +46,29 @@ export default {
 }
 </script>
 <style lang='less' scoped>
+.home_container {
+  height: 100%;
+}
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+  div {
+    display: flex;
+    align-items: center;
+    img {
+      margin: 0 10px;
+    }
+  }
+}
+.el-aside {
+  background-color: #333744;
+}
+.el-main {
+  background-color: #eaedf1;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -8,7 +8,12 @@ import axios from 'axios'
 Vue.prototype.$http = axios
 // 请求根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
-
+axios.interceptors.request.use(config => {
+  console.log(config)
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  // 必须 return
+  return config
+})
 Vue.config.productionTip = false
 
 new Vue({
