d8955e461f8f9399eb5d5f3fc696b99984436d01
“Vimalate <1902644369@qq.com>
2020-01-09 01:34:54 +0800

    编辑用户表单

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -1,16 +1,254 @@
 <template>
-  <div>user</div>
+  <div>
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片视图 -->
+    <el-card class="box-card">
+      <el-row :gutter="20">
+        <el-col :span="8">
+          <el-input placeholder="请输入内容" v-model="queryInfo.query" clearable @clear="getUserList">
+            <el-button slot="append" icon="el-icon-search" @click="getUserList"></el-button>
+          </el-input>
+        </el-col>
+        <el-col :span="4">
+          <el-button type="primary" @click="dialogVisible=true">添加用户</el-button>
+        </el-col>
+      </el-row>
+      <!-- 用户列表 -->
+      <el-table :data="userList" border stripe>
+        <el-table-column label="#" type="index"></el-table-column>
+        <el-table-column label="姓名" prop="username"></el-table-column>
+        <el-table-column label="邮箱" prop="email"></el-table-column>
+        <el-table-column label="电话" prop="mobile"></el-table-column>
+        <el-table-column label="角色" prop="role_name"></el-table-column>
+        <el-table-column label="状态" prop="mg_state">
+          <template slot-scope="scope">
+            <el-switch v-model="scope.row.mg_state" @change="userStateChanged(scope.row)"></el-switch>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" width="180px">
+          <template slot-scope="scope">
+            <!-- 修改按钮 -->
+            <el-button
+              type="primary"
+              icon="el-icon-edit"
+              size="mini"
+              @click="showEditDialog(scope.row.id)"
+            ></el-button>
+            <!-- 删除按钮 -->
+            <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+            <!-- 分配角色 -->
+            <el-tooltip effect="dark" content="分配角色" placement="top" :enterable="false">
+              <el-button type="warning" icon="el-icon-setting" size="mini"></el-button>
+            </el-tooltip>
+          </template>
+        </el-table-column>
+      </el-table>
+      <!-- 分页区域 -->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.pagenum"
+        :page-sizes="[1, 2, 5, 10]"
+        :page-size="queryInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+      ></el-pagination>
+    </el-card>
+
+    <!-- 添加用户对话框 -->
+    <el-dialog title="提示" :visible.sync="dialogVisible" width="50%" @close="addDialogClosed">
+      <!-- 主体内容区域 -->
+      <el-form :model="addFrom" :rules="addFromRules" ref="ruleForm" label-width="70px">
+        <el-form-item label="用户名" prop="username">
+          <el-input v-model="addFrom.username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="addFrom.password"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" prop="email">
+          <el-input v-model="addFrom.email"></el-input>
+        </el-form-item>
+        <el-form-item label="手机" prop="mobile">
+          <el-input v-model="addFrom.mobile"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addUser">确 定</el-button>
+      </span>
+    </el-dialog>
+
+    <!-- 修改用户对话框 -->
+    <el-dialog title="修改用户" :visible.sync="editDialogVisible" width="50%">
+      <span>这是一段信息</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="editDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="editDialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
+    var checkEmail = (rule, value, cb) => {
+      const regEmail = /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(\.[a-zA-Z0-9_-])+/
+      if (regEmail.test(value)) {
+        // 合法邮箱
+        return cb()
+      }
+      cb(new Error('请输入合法邮箱'))
+    }
+    var checkMobile = (rule, value, cb) => {
+      // 验证手机号的正则表达式
+      const regMobile = /^(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$/
+
+      if (regMobile.test(value)) {
+        return cb()
+      }
+
+      cb(new Error('请输入合法的手机号'))
+    }
     return {
+      // 获取用户列表的参数对象
+      queryInfo: {
+        query: '',
+        // 当前页数
+        pagenum: 1,
+        // 当前显示每页
+        pagesize: 2
+      },
+      userList: [],
+      total: 0,
+      // 用户对话框是否显示
+      dialogVisible: false,
+      // 添加用户表单
+      addFrom: {
+        username: '',
+        email: '',
+        password: '',
+        mobile: ''
+      },
+      // 控制修改用户对话框显示
+      editDialogVisible: false,
+      // 查询到的用户信息对象
+      editFrom: {},
+      // 添加表单验证规则
+      addFromRules: {
+        username: [
+          {
+            required: true,
+            message: '请输入用户名',
+            trigger: 'blur'
+          },
+          {
+            min: 3,
+            max: 10,
+            message: '用户名长度在3~10个之间',
+            trigger: 'blur'
+          }
+        ],
+        password: [
+          {
+            required: true,
+            message: '请输入密码',
+            trigger: 'blur'
+          },
+          {
+            min: 6,
+            max: 15,
+            message: '用户名长度在6~15个之间',
+            trigger: 'blur'
+          }
+        ],
+        email: [
+          {
+            required: true,
+            message: '请输入邮箱',
+            trigger: 'blur'
+          },
+          { validator: checkEmail, trigger: 'blur' }
+        ],
+        mobile: [
+          {
+            required: true,
+            message: '请输入手机',
+            trigger: 'blur'
+          },
+          { validator: checkMobile, trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  created() {
+    this.getUserList()
+  },
+  methods: {
+    async getUserList() {
+      const { data: res } = await this.$http.get('users', {
+        params: this.queryInfo
+      })
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取用户列表失败！')
+      }
+      this.userList = res.data.users
+      this.total = res.data.total
+    },
+    // 监听 pagesize 改变的事件
+    handleSizeChange(newSize) {
+      this.queryInfo.pagesize = newSize
+      this.getUserList()
+    },
+    // 监听页码值改变
+    handleCurrentChange(newPage) {
+      this.queryInfo.pagenum = newPage
+      this.getUserList()
+    },
+    // 监听 switch 开关状态
+    async userStateChanged(userinfo) {
+      console.log(userinfo)
+      const { data: res } = await this.$http.put(
+        `users/${userinfo.id}/state/${userinfo.mg_state}`
+      )
+      if (res.meta.status !== 200) {
+        userinfo.mg_state = !userinfo.mg_state
+        return this.$message.error('更新用户状态失败')
+      }
+      this.$message.success('更新用户状态成功！')
+    },
+    addDialogClosed() {
+      this.$refs.ruleForm.resetFields()
+    },
+    addUser() {
+      this.$refs.ruleForm.validate(async valid => {
+        if (!valid) return
+        // 校验通过则添加
+        const { data: res } = await this.$http.post('users', this.addFrom)
+        if (res.metastatus !== 201) {
+          this.$message.error('添加用户失败！')
+        }
+        this.$message.success('添加用户成功！')
+        // 隐藏对话框
+        this.dialogVisible = false
+        // 从新获取用户列表
+      })
+    },
+    // 展示编辑用户对话框
+    async showEditDialog(id) {
+      const { data: res } = await this.$http.get('users/' + id)
+      if (res.meta.status !== 200) {
+        this.$message.error('查询用户信息失败！')
+      }
+      this.editFrom = res.data
+      this.editDialogVisible = true
     }
   }
-
 }
-
 </script>
 <style lang='less' scoped>
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,12 +1,23 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main, Menu, Submenu, MenuItem, MenuItemGroup } from 'element-ui'
+import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main, Menu, Submenu, MenuItem, MenuItemGroup, Breadcrumb, BreadcrumbItem, Card, Row, Col, Table, TableColumn, Switch, Tooltip, Pagination, Dialog } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
+Vue.use(Pagination)
+Vue.use(Dialog)
+Vue.use(Tooltip)
+Vue.use(Table)
+Vue.use(Switch)
+Vue.use(TableColumn)
 Vue.use(FormItem)
+Vue.use(Breadcrumb)
+Vue.use(Card)
+Vue.use(BreadcrumbItem)
 Vue.use(Input)
 Vue.use(Container)
 Vue.use(Main)
+Vue.use(Row)
+Vue.use(Col)
 Vue.use(Header)
 Vue.use(Aside)
 Vue.use(Menu)
